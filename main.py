c582548d57e9b59306689b989a96f39873abc6a1
Lubomir Naydenov <lubonay@gmail.com>
2024-03-22 15:54:43 +0200

    Solve a maze in main.py
    Also configured the maze to fit the full 800x600 window.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from maze import Maze
 def main():
     win = Window(800, 600)
 
-    maze = Maze(10, 10, 10, 10, 30, win)
+    maze = Maze(12, 18, 26, 19, 30, win)
     maze.generate_paths()
+    maze.solve()
 
     win.wait_for_close()
 
